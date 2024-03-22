f540a5d31a5a19dd3727c34e9d85041089a6f39c
Akshit shubham <akshitshubhammas@gmail.com>
2024-03-22 18:26:02 +0530

    Update v2 + fixed en.m4a issue

--- merge.py
+++ merge.py
@@ -1,5 +1,6 @@
+from process import shell
 class Merge:
     def ffmpegMerge(self,input1,input2,output,ffmpeg_path="ffmpeg"):
-        import os
-        os.system(f'{ffmpeg_path} -i {input1} -i {input2} -c copy {output}')
+
+        shell(f'{ffmpeg_path} -i {input1} -i {input2} -c copy {output}',r'^\[out#0\/mp4')
         return output
