1b6c2c84e8bdcb7daa467b8da27037c07b580584
Akshit shubham <121078566+shubhamakshit@users.noreply.github.com>
2024-03-22 18:39:46 +0000

    update Flare

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Download defaults.json from the given URL and save as "defaults.json"
+curl -o defaults.json https://raw.githubusercontent.com/shubhamakshit/pwdlv3/main/defaults.linux.json
+
+# Rest of your setup script goes here
+# ...
