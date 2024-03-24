a3ffa272d6870b438930c97c1a18d5616f3efdc1
Akshit shubham <121078566+shubhamakshit@users.noreply.github.com>
2024-03-24 17:53:59 +0000

    Minor bug fixes and Code cleanup

--- decrypt.py
+++ decrypt.py
@@ -2,39 +2,50 @@ from glv import Global
 from process import shell
 from basicUtils import BasicUtils
 import error 
+import os
 
 class Decrypt:
-    def decryptAudio(self,path,name,key,mp4d="mp4decrypt",verbose=True):
-        
+
+    def decrypt(self,path,name,key,mp4d="mp4decrypt",out="None",verbose=True):
+
+        # making path absolute if not already absolute
         path = BasicUtils.abspath(path)
-        Global.dprint("Decrypting Audio...")
+        Global.dprint(f"Decrypting {out}...")
 
-        if verbose: Global.dprint("Audio Decryption Started..."); Global.dprint(f'{mp4d} --key 1:{key} {path}/{name}.m4a {path}/audio.mp4')
+        # during various tests
+        # it was found that the decrypted audio file is named as <name>.en.m4a 
+        # hence a simple logic to work around this issue is to check if the file exists
+        if not os.path.exists(f'{path}/{name}.m4a') and out == "Audio":
+            name = name + ".en"
 
-        code = shell(f'{mp4d} --key 1:{key} {path}/{name}.m4a {path}/audio.mp4 ')
+        # setting extension based on out
+        # i.e if out is Audio then extension is 'm4a' else 'mp4'
+        extension = "m4a" if out == "Audio" else "mp4"
+
+        decrypt_command = f'{mp4d} --key 1:{key} {path}/{name}.{extension} {path}/{out}.mp4'
+
+        if verbose: Global.dprint(f"{out} Decryption Started..."); Global.dprint(f'{decrypt_command}')
+
+        
+
+        # the main part where the decryption happens
+        code = shell(f'{decrypt_command}',stderr="",stdout="")
+
+        # simple check to see if the decryption was successful or not
         if code == 0:
-            Global.dprint("Audio Decrypted Successfully")
+            Global.dprint(f"{out} Decrypted Successfully")
         else:
-            if code == 1:
-                code = shell(f'mp4decrypt --key 1:{key} {path}/{name}.en.m4a {path}/audio.mp4 ')
-                if code == 0:
-                    Global.dprint("Audio Decrypted Successfully")
-                else:
-                    error.errorList["couldNotDecryptAudio"]["func"]()
-                    exit(error.errorList["couldNotDecryptAudio"]["code"])
+
+            # if decryption failed then print error message and exit
+            error.errorList[f"couldNotDecrypt{out}"]["func"]()
+            exit(error.errorList[f"couldNotDecrypt{out}"]["code"])
+
         Global.hr()
 
+    # decrypts audio
+    def decryptAudio(self,path,name,key,mp4d="mp4decrypt",verbose=True):
+        self.decrypt(path,name,key,mp4d,"Audio",verbose)
+
+    # decrypts video
     def decryptVideo(self,path,name,key,mp4d="mp4decrypt",verbose=True):
-            
-            path = BasicUtils.abspath(path)
-            Global.dprint("Decrypting Video...")
-
-            if verbose: Global.dprint("Video Decryption Started..."); Global.dprint(f'{mp4d} --key 1:{key} {path}/{name}.mp4 {path}/video.mp4')
-
-            code = shell(f'{mp4d} --key 1:{key} {path}/{name}.mp4 {path}/video.mp4 ',r'^Decoding')
-            if code == 0:
-                Global.dprint("Video Decrypted Successfully")
-            else:
-                error.errorList["couldNotDecryptVideo"]["func"]()
-                exit(error.errorList["couldNotDecryptVideo"]["code"])
-            Global.hr()
+        self.decrypt(path,name,key,mp4d,"Video",verbose)
