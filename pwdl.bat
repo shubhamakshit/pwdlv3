3129dd36790085ec3294f0f1d9a809364ff2fedc
Akshit shubham <121078566+shubhamakshit@users.noreply.github.com>
2024-06-05 15:00:10 +0530

    Update pwdl.bat

--- pwdl.bat
+++ pwdl.bat
@@ -2,11 +2,11 @@
 REM Batch script for running the Python script pwdl.py
 
 REM Check if Python is installed
-python --version >nul 2>&1
+py --version >nul 2>&1
 if errorlevel 1 (
     echo Python is not installed or not found in the PATH.
     exit /b 1
 )
 
 REM Run the Python script with the provided arguments
-python "%~dp0pwdl.py" %*
+py "%~dp0pwdl.py" %*
