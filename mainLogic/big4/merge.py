9dafd56bb10a1dd59f7e3f421bc3e2df4e745abd
Akshit shubham <akshitshubhammas@gmail.com>
2024-07-01 11:48:56 +0530

    minor bug fixes

--- mainLogic/big4/merge.py
+++ mainLogic/big4/merge.py
@@ -2,29 +2,35 @@ import os
 from mainLogic.error import errorList
 from mainLogic.utils.process import shell
 from mainLogic.utils.glv import Global
+from mainLogic.utils.os2 import SysFunc
 class Merge:
+
+    def mergeCommandBuilder(self,ffmpeg_path,input1,input2,output,overwrite=False):
+
+        return f'{ffmpeg_path} {'-y' if overwrite else ''} -i {input1} -i {input2} -c copy {output}'
+
+
     def ffmpegMerge(self,input1,input2,output,ffmpeg_path="ffmpeg",verbose=False):
 
+        input1,input2,output = SysFunc.modify_path(input1),SysFunc.modify_path(input2),SysFunc.modify_path(output)
 
         if verbose: Global.hr();Global.dprint('Attempting ffmpeg merge')
-        if verbose: Global.dprint(f'{ffmpeg_path} -i {input1} -i {input2} -c copy {output}')
+        # if verbose: Global.dprint(f'{ffmpeg_path} -i {input1} -i {input2} -c copy {output}')
+
+        if os.path.exists(output):
+
+            Global.errprint("Warninbg: Output file already exists. Overwriting...")
+            consent = input("Do you want to continue? (y/n): ")
+
+            if consent.lower() != 'y':
+                errorList['overWriteAbortedByUser']['func']()
+                exit(errorList['overWriteAbortedByUser']['code'])
 
         if verbose:
-
-            shell(f'{ffmpeg_path} -i {input1} -i {input2} -c copy {output}')
-
+            Global.dprint(f"Running: {self.mergeCommandBuilder(ffmpeg_path,input1,input2,output,overwrite=True)}")
+            shell(self.mergeCommandBuilder(ffmpeg_path,input1,input2,output,overwrite=True),filter='.*')
         else:
+            shell(self.mergeCommandBuilder(ffmpeg_path,input1,input2,output,overwrite=True), stderr="", stdout="")
 
-            if os.path.exists(output):
-
-                Global.errprint("Warninbg: Output file already exists. Overwriting...")
-                consent = input("Do you want to continue? (y/n): ")
-
-                if consent.lower() != 'y':
-                    errorList['overWriteAbortedByUser']['func']()
-                    exit(errorList['overWriteAbortedByUser']['code'])
-
-                else:
-                    shell(f'{ffmpeg_path} -y -i {input1} -i {input2} -c copy {output}',stderr="",stdout="")
 
         return output
--- pwdl.py
+++ pwdl.py
@@ -1,9 +1,10 @@
 import argparse
+import sys
+import os
+
 from mainLogic.error import errorList
 from mainLogic.utils.glv import Global
-import sys
 from mainLogic.utils.os2 import SysFunc
-import os
 from mainLogic.main import Main
 from beta.shellLogic import shell
 from mainLogic.startup.checkup import CheckState
@@ -13,166 +14,135 @@ from mainLogic.utils import glv_var
 # global variables
 prefs = {}
 glv = Global()
-os2 = SysFunc()
 
 # hardcoding the list of executables required for the script to run
-# should be available in the PATH or the user should provide the path to the executables
 EXECUTABLES = glv_var.EXECUTABLES
 
 
-
-
-def main():
-
-    # parsing the arguments
+def parse_arguments():
+    """Parse command-line arguments."""
     parser = argparse.ArgumentParser(description='PhysicsWallah M3u8 parser.')
-
     parser.add_argument('--csv-file', type=str, help='Input csv file. Legacy Support too.')
     parser.add_argument('--id', type=str,
-                        help='PhysicsWallh Video Id for single usage. Incompatible with --csv-file.   Must be used '
-                             'with --name')
+                        help='PhysicsWallh Video Id for single usage. Incompatible with --csv-file. Must be used with --name')
     parser.add_argument('--name', type=str,
-                        help='Name for the output file. Incompatible with --csv-file.   Must be used with --id')
+                        help='Name for the output file. Incompatible with --csv-file. Must be used with --id')
     parser.add_argument('--dir', type=str, help='Output Directory')
     parser.add_argument('--verbose', action='store_true', help='Verbose Output')
-    parser.add_argument('--shell',action='store_true',help='Start the shell')
+    parser.add_argument('--shell', action='store_true', help='Start the shell')
     parser.add_argument('--webui', nargs='?', const=-1, type=int, help='Start the Webui')
     parser.add_argument('--version', action='version', version='%(prog)s 1.0')
     parser.add_argument('--simulate', action='store_true',
-                        help='Simulate the download process. No files will be downloaded.)')
+                        help='Simulate the download process. No files will be downloaded.')
+    return parser.parse_args()
 
-    args = parser.parse_args()
+
+def start_shell():
+    """Start the shell if requested."""
+    shell.main()
+
+
+def check_dependencies(directory, verbose):
+    """Check if all dependencies are installed."""
+    global prefs
+    state = CheckState().checkup(EXECUTABLES, directory=directory, verbose=verbose)
+    prefs = state['prefs']
+    return state
+
+
+def start_webui(port, verbose):
+    """Start the WebUI if requested."""
+    from run import app
+    if not prefs['webui']:
+        Global.errprint("WebUI is not enabled in the preferences. Exiting ...")
+        sys.exit(1)
+
+    if 'webui-port' in prefs:
+        port = prefs['webui-port']
+
+    if verbose:
+        Global.hr()
+        Global.dprint(f"Starting WebUI on port {port}")
+
+    app.run(host="0.0.0.0", debug=True, port=port)
+
+
+def download_process(id, name, state, verbose, simulate=False):
+    """Process a single download or simulate the download."""
+    if simulate:
+        print("Simulating the download process. No files will be downloaded.")
+        print(f"Id to be processed: {id}")
+        print(f"Name to be processed: {name}")
+        return
+
+    try:
+        Main(
+            id=id,
+            name=generate_safe_folder_name(name),
+            directory=prefs['dir'],
+            ffmpeg=state['ffmpeg'],
+            vsdPath=state['vsd'],
+            token=prefs['token'],
+            mp4d=state['mp4decrypt'],
+            tmpDir=prefs['tmpDir'],
+            verbose=verbose
+        ).process()
+    except Exception as e:
+        if verbose:
+            Global.hr()
+            glv.errprint(f"Error: {e}")
+        errorList['downloadFailed']['func'](name, id)
+        sys.exit(errorList['downloadFailed']['code'])
+
+
+def handle_csv_file(csv_file, state, verbose, simulate=False):
+    """Handle processing of CSV file."""
+    if not os.path.exists(csv_file):
+        errorList['csvFileNotFound']['func'](csv_file)
+        sys.exit(errorList['csvFileNotFound']['code'])
+
+    if simulate:
+        print("Simulating the download csv process. No files will be downloaded.")
+        print(f"File to be processed: {csv_file}")
+        return
+
+    with open(csv_file, 'r') as f:
+        for line in f:
+            name, id = line.strip().split(',')
+            name = generate_safe_folder_name(name)
+            download_process(id, name, state, verbose)
+
+
+def main():
+    args = parse_arguments()
 
     if args.shell:
-        shell.main()
+        start_shell()
 
-
-
-    # user_input is given preference i.e if --verbose is true it will override
-    # however if --verbose is false but prefs['verbose'] is true
     glv.vout = args.verbose
 
-    global prefs
-
-    # check if all dependencies are installed
-    state = CheckState().checkup(EXECUTABLES, directory=args.dir,verbose=glv.vout)
-    prefs = state['prefs']
+    state = check_dependencies(args.dir, glv.vout)
 
     if args.webui:
-        from run import app
-        if not prefs['webui']:
-            Global.errprint("WebUI is not enabled in the preferences. Exiting ...")
-            sys.exit(1)
+        start_webui(args.webui, glv.vout)
 
-        if 'webui-port' in prefs:
-            port = prefs['webui-port']
-        else:
-            port = 5000
+    if args.simulate:
+        if args.csv_file:
+            handle_csv_file(args.csv_file, state, glv.vout, simulate=True)
+        elif args.id and args.name:
+            download_process(args.id, args.name, state, glv.vout, simulate=True)
+        sys.exit(0)
 
-        try:
-            if args.webui != -1 :
-                port = int(args.webui)
-            else:
-                pass
-        except:
-            pass
-
-        if glv.vout: Global.hr(); Global.dprint(f"Starting WebUI on port {port}")
-
-        app.run(host="0.0.0.0",debug=True,port=port)
-
-    # --------------------------------------------------------------------------------------------------------------------------------------
-    # setting verbose output
-    # gives preference to user input
-    if not glv.vout and prefs['verbose']: glv.vout = prefs['verbose']
-
-    verbose = glv.vout
-
-    OUT_DIRECTORY = prefs['dir']
-    if verbose: Global.hr(); glv.dprint(f"Tmp Dir is: {SysFunc.modify_path(prefs['tmpDir'])}")
-    if verbose: Global.hr(); glv.dprint(f'Output Directory: {OUT_DIRECTORY}')
-    if verbose: Global.hr(); glv.dprint(f"Horizontal Rule: {not Global.disable_hr}")
-
-    # --------------------------------------------------------------------------------------------------------------------------------------
-    # end of loading user preferences
-
-
-    # starting the main process
-
-    #if both csv file and (id or name) is provided then -> exit with error code 3
     if args.csv_file and (args.id or args.name):
         print("Both csv file and id (or name) is provided. Unable to decide. Aborting! ...")
         sys.exit(3)
 
-    # handle in case --csv-file is provided
     if args.csv_file:
-
-        # simulation mode
-        if args.simulate:
-            print("Simulating the download csv process. No files will be downloaded.")
-            print("File to be processed: ", args.csv_file)
-            exit(0)
-
-        # exiting in case the CSV File is not found
-        if not os.path.exists(args.csv_file):
-            errorList['csvFileNotFound']['func'](args.csv_file)
-            sys.exit(errorList['csvFileNotFound']['code'])
-
-        with open(args.csv_file, 'r') as f:
-            for line in f:
-                name, id = line.strip().split(',')
-                name = generate_safe_folder_name(name)
-
-                # adding support for csv file with partial errors
-                try:
-                    Main(id=id,
-                         name=name,
-                         directory=OUT_DIRECTORY,
-                         ffmpeg=state['ffmpeg'],
-                         vsdPath=state['vsd'],
-                         token=prefs['token'],
-                         mp4d=state['mp4decrypt'],
-                         tmpDir=prefs['tmpDir'],
-                         verbose=verbose,
-                         suppress_exit=True  # suppress exit in case of error (as multiple files are being processed)
-                         ).process()
-
-                except Exception as e:
-                    if verbose: Global.hr(); glv.errprint(f"Error: {e}")
-                    errorList['downloadFailed']['func'](name, id)
-
-
-
-    # handle in case key and name is given
+        handle_csv_file(args.csv_file, state, glv.vout)
     elif args.id and args.name:
-
-        # simulation mode
-        if args.simulate:
-            print("Simulating the download process. No files will be downloaded.")
-            print("Id to be processed: ", args.id)
-            print("Name to be processed: ", args.name)
-            exit(0)
-
-        try:
-
-            Main(id=args.id,
-                 name=generate_safe_folder_name(args.name),
-                 directory=OUT_DIRECTORY,
-                 ffmpeg=state['ffmpeg'],
-                 vsdPath=state['vsd'],
-                 token=prefs['token'],
-                 mp4d=state['mp4decrypt'],
-                 tmpDir=prefs['tmpDir'],
-                 verbose=verbose).process()
-
-        except Exception as e:
-            if verbose: Global.hr(); glv.errprint(f"Error: {e}")
-            errorList['downloadFailed']['func'](args.name, args.id)
-            sys.exit(errorList['downloadFailed']['code'])
-
-    # in case neither is used
+        download_process(args.id, args.name, state, glv.vout)
     else:
-        exit(1)
+        sys.exit(1)
 
 
 if __name__ == "__main__":
