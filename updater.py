3d83229740140e9d53cebfbf59596872435daecf
Akshit shubham <akshitshubhammas@gmail.com>
2024-07-06 23:53:22 +0530

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,29 +1,30 @@
+import subprocess
 from mainLogic.utils.process import shell
 
 def stash_files(files):
     files_str = ' '.join(files)
     stash_command = f"git stash push -m 'Stash local changes for {files_str}' {files_str}"
     result = shell(stash_command)
-    if result.returncode != 0:
-        print(f"Command '{stash_command}' failed with error:\n{result.stderr}")
+    if result:
+        print(f"Command '{stash_command}' succeeded with output:\n{result}")
     else:
-        print(f"Command '{stash_command}' succeeded with output:\n{result.stdout}")
+        print(f"Command '{stash_command}' failed.")
 
 def pull_changes(branch='main'):
     pull_command = f"git pull origin {branch}"
     result = shell(pull_command)
-    if result.returncode != 0:
-        print(f"Command '{pull_command}' failed with error:\n{result.stderr}")
+    if result:
+        print(f"Command '{pull_command}' succeeded with output:\n{result}")
     else:
-        print(f"Command '{pull_command}' succeeded with output:\n{result.stdout}")
+        print(f"Command '{pull_command}' failed.")
 
 def apply_stash():
     apply_command = "git stash pop"
     result = shell(apply_command)
-    if result.returncode != 0:
-        print(f"Command '{apply_command}' failed with error:\n{result.stderr}")
+    if result:
+        print(f"Command '{apply_command}' succeeded with output:\n{result}")
     else:
-        print(f"Command '{apply_command}' succeeded with output:\n{result.stdout}")
+        print(f"Command '{apply_command}' failed.")
 
 def update_repo(files_to_stash, branch='main'):
     stash_files(files_to_stash)
