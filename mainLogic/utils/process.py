5b223cb684edb416625a35842891afe28750d2aa
Akshit shubham <akshitshubhammas@gmail.com>
2024-07-07 10:26:32 +0530

    Update

--- mainLogic/utils/process.py
+++ mainLogic/utils/process.py
@@ -1,8 +1,8 @@
 import subprocess
 import re
 import sys
-
-def shell(command, filter=None, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, progress_callback=None, handleProgress=None, inline_progress=False):
+import os
+def shell(command, filter=None, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, progress_callback=None, handleProgress=None, inline_progress=False, return_out=False,cwd=os.getcwd()):
     import os
 
     # Set PYTHONUNBUFFERED environment variable
@@ -10,49 +10,58 @@ def shell(command, filter=None, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
 
     command = to_list(command)
 
- 
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf-8', universal_newlines=True)
+    try:
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf-8', universal_newlines=True, cwd=cwd)
 
-    while True:
-        try:
-            output = process.stdout.readline()
-            output = output.strip()
+        output_lines = []
+        while True:
+            try:
+                output = process.stdout.readline()
+                if output == '' and process.poll() is not None:
+                    break
 
-            if output == '' and process.poll() is not None:
-                break
+                output = output.strip()
+                if output:
+                    output_lines.append(output)
 
-            if output:
-                if filter is not None and re.search(filter, output):
-                    # Call the progress callback with the filtered output
-                    if progress_callback:
-                        if handleProgress:
-                            progress_callback(handleProgress(output))
-                        else:
-                            progress_callback(output)
+                    if filter is not None and re.search(filter, output):
+                        # Call the progress callback with the filtered output
+                        if progress_callback:
+                            if handleProgress:
+                                progress_callback(handleProgress(output))
+                            else:
+                                progress_callback(output)
 
-                if inline_progress:
-                    # Update progress in the same line
-                    sys.stdout.write('\r' + output.strip())
-                    sys.stdout.flush()
-                else:
-                    # Print output normally
-                    if not stdout == '' or not stdout == None:
-                        continue
-                    print(output)
+                    if inline_progress:
+                        # Update progress in the same line
+                        sys.stdout.write('\r' + output.strip())
+                        sys.stdout.flush()
+                    else:
+                        # Print output normally
+                        if stdout is not None and stdout != '':
+                            print(output)
 
+            except UnicodeEncodeError:
+                sys.stdout.write("\rUnicodeEncodeError")
+                sys.stdout.flush()
+                pass
 
+        # Wait for the process to complete and get the return code
+        return_code_value = process.wait()
 
-        except UnicodeEncodeError:
-            sys.stdout.write("\rUnicodeEncodeError")
-            sys.stdout.flush()
-            pass
-        except Exception as e:
-            print(f"Error: {e}")
+        # Return output only if return_code flag is False
+        if not return_out:
+            return return_code_value
+        else:
+            # Return both output and return code
+            return return_code_value, output_lines
 
-    # Wait for the process to complete and get the return code
-    return_code = process.wait()
-
-    return return_code
+    except Exception as e:
+        print(f"Error: {e}")
+        if not return_out:
+            return []
+        else:
+            return -1, []
 
 def to_list(variable):
     if isinstance(variable, list):
